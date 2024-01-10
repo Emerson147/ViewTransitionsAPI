a216f8369c9c42df6da91d74d1867dcc14ee5360
Emerson Rafael <emersontec147@gmail.com>
2024-01-10 02:36:45 -0500

    ViewTransitionApi

---
+++ src/data/books.js
@@ -0,0 +1,44 @@
+export const books = [
+	{
+		id: 'clean-code',
+		title: 'Clean Code: Manual de estilo para el desarrollo ágil de software',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1436202607i/3735293.jpg',
+		opinion: 'Even bad code can function. But if code isn’t clean, it can bring a development organization to its knees. Every year, countless hours and significant resources are lost because of poorly written code. But it doesn’t have to be that way.',
+		author: 'Robert C. Martin'
+	},
+	{
+		id: 'pragmatic-programmer',
+		title: 'The Pragmatic Programmer: From Journeyman to Master',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1401432508i/4099.jpg',
+		opinion: 'Straight from the programming trenches, The Pragmatic Programmer cuts through the increasing specialization and technicalities of modern software development to examine the core process--taking a requirement and producing working, maintainable code that delights its users. It covers topics ranging from personal responsibility and career development to architectural techniques for keeping your code flexible and easy to adapt and reuse. ',
+		author: 'Andrew Hunt, David Thomas'
+	},
+	{
+		id: 'refactoring',
+		title: 'Refactoring: Improving the Design of Existing Code',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1386925632i/44936.jpg',
+		opinion: 'As the application of object technology—particularly the Java programming language—has become commonplace, a new problem has emerged to confront the software development community. Significant numbers of poorly designed programs have been created by less-experienced developers, resulting in applications that are inefficient and hard to maintain and extend. Increasingly, software system professionals are discovering just how difficult it is to work with these inherited, non-optimal applications.',
+		author: 'Martin Fowler'
+	},
+	{
+		id: 'design-patterns',
+		title: 'Design Patterns: Elements of Reusable Object-Oriented Software',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1348027904i/85009.jpg',
+		opinion: 'Capturing a wealth of experience about the design of object-oriented software, four top-notch designers present a catalog of simple and succinct solutions to commonly occurring design problems. Previously undocumented, these 23 patterns allow designers to create more flexible, elegant, and ultimately reusable designs without having to rediscover the design solutions themselves.',
+		author: 'Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides'
+	},
+	{
+		id: 'code-complete',
+		title: 'Code Complete: A Practical Handbook of Software Construction',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1396837641i/4845.jpg',
+		opinion: 'Widely considered one of the best practical guides to programming, Steve McConnell’s original CODE COMPLETE has been helping developers write better software for more than a decade. Now this classic book has been fully updated and revised with leading-edge practices—and hundreds of new code samples—illustrating the art and science of software construction. Capturing the body of knowledge available from research, academia, and everyday commercial practice, McConnell synthesizes the most effective techniques and must-know principles into clear, pragmatic guidance. No matter what your experience level, development environment, or project size, this book will inform and stimulate your thinking—and help you build the highest quality code.',
+		author: 'Steve McConnell'
+	},
+	{
+		id: 'javascript-good',
+		title: 'JavaScript: The Good Parts',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1328834793i/2998152.jpg',
+		opinion: 'Most programming languages contain good and bad parts, but JavaScript has more than its share of the bad, having been developed and released in a hurry before it could be refined. This authoritative book scrapes away these bad features to reveal a subset of JavaScript that\'s more reliable, readable, and maintainable than the language as a whole—a subset you can use to create truly extensible and efficient code.',
+		author: 'Douglas Crockford'
+	}
+]
